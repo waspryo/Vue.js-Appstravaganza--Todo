69a10a44d3b928ba51ad5c33e21e109c243a43e7
waspryo <wasp2123321@gmail.com>
2019-12-09 23:31:30 +0900

    changed form

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,15 @@
 <template>
   <div id="app" class="container">
-    <h1>todo App</h1>
+    <h1 class="text-center">todo App</h1>
+    <form>
+  <div class="form-group">
+    <label for="newTodo">New Todo</label>
+    <input type="text" class="form-control" id="newTodo" aria-describedby="newTodoHelp" placeholder="walk the dog...">
+    <small id="newTodoHelp" class="form-text text-muted">Enter a new Todo</small>
+  </div>
+  <button type="submit" class="btn btn-primary">Add todo</button>
+</form>
+
   </div>
 </template>
 
