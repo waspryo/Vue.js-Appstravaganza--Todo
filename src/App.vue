c4157a3a2c21f9ef066b9879ea60003e8abb7e29
waspryo <wasp2123321@gmail.com>
2019-12-09 23:41:46 +0900

    submit by prevent

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,36 @@
 <template>
   <div id="app" class="container">
     <h1 class="text-center">todo App</h1>
-    <form>
-  <div class="form-group">
-    <label for="newTodo">New Todo</label>
-    <input type="text" class="form-control" id="newTodo" aria-describedby="newTodoHelp" placeholder="walk the dog...">
-    <small id="newTodoHelp" class="form-text text-muted">Enter a new Todo</small>
-  </div>
-  <button type="submit" class="btn btn-primary">Add todo</button>
-</form>
-
+    <form @submit.prevent="addTodo()">
+      <div class="form-group">
+        <label for="newTodo">New Todo</label>
+        <input
+          type="text"
+          class="form-control"
+          id="newTodo"
+          aria-describedby="newTodoHelp"
+          placeholder="walk the dog..."
+        />
+        <small id="newTodoHelp" class="form-text text-muted">Enter a new Todo</small>
+      </div>
+      <button type="submit" class="btn btn-primary">Add todo</button>
+    </form>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app',
-  data () {
-    return {
-      
+  name: "app",
+  data() {
+    return {};
+  },
+  methods: {
+    addTodo() {
+      console.log('form submitted')
     }
   }
-}
+};
 </script>
 
 <style>
-
 </style>
